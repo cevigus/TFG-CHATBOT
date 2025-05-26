7099f10b02d4afb490bc259ce8cfd7c6d64f530a
cevigus <trabajofinaltfg454@gmail.com>
2025-05-26 23:01:21 +0200

    Modificacion de puertos

--- index.js
+++ index.js
@@ -225,9 +225,11 @@ const edadStats = await Usuario.aggregate([
 
 
 // Iniciar el servidor
-app.listen(3000, () => {
-  console.log('Servidor iniciado en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
+
 
 
 
